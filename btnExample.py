6db184452407f95ceefd4d603b6face14d608aa5
malan <malanakgames@seznam.cz>
2022-10-21 16:13:19 +0200

    Better comments for btnExample.py

--- btnExample.py
+++ btnExample.py
@@ -5,17 +5,15 @@ import discord
 from dotenv import load_dotenv
 load_dotenv()
 
-
-print("y")
-class Buttons(discord.ui.View):
+class Buttons(discord.ui.View): # This class inherits from discord.ui.View so it can handle them
     global view
-    def __init__(self, *, timeout=180):
+    def __init__(self, *, timeout=180): # Maybe needed, maybe not but it shouldn't cause any harm
         super().__init__(timeout=timeout)
     
-    @discord.ui.button(label="Red button",style=discord.ButtonStyle.red)
-    async def red_btn(self, ctx: discord.Interaction, button: discord.ui.Button):
-            view=Buttons() # So you can remove the buttons
-            view.clear_items() # removes the buttons
+    @discord.ui.button(label="Red button",style=discord.ButtonStyle.red) # Specifies the button name, etc
+    async def red_btn(self, ctx: discord.Interaction, button: discord.ui.Button): # ctx here is mainly about the person who clicked the button, the name has to be original in the context of buttons, if there are multiple with the same name, they WILL conflict
+            view=Buttons() # Makes it so you can do edit the view
+            view.clear_items() # Removes the buttons
             embeddedResponse = discord.Embed(title="Embed title", description="Clicked red button", color=discord.Color.red())
             await ctx.response.edit_message(embed=embeddedResponse, view=view)
     @discord.ui.button(label="Green button",style=discord.ButtonStyle.green)
